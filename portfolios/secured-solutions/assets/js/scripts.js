03cdd6522b62a981e653b57a660a86c3e771eeaf
asrafuzzaman <online.knock@gmail.com>
2019-01-11 21:12:58 +0600

    update scripts file

--- portfolios/secured-solutions/assets/js/scripts.js
+++ portfolios/secured-solutions/assets/js/scripts.js
@@ -22,17 +22,6 @@ $(function () {
       margin:0,
       nav:false,
       dots: false,
-      // responsive:{
-      //     0:{
-      //         items:1
-      //     },
-      //     600:{
-      //         items:3
-      //     },
-      //     1000:{
-      //         items:5
-      //     }
-      // }
   })
   $('.ss-brand-logo-slider').owlCarousel({
       items: 6,
